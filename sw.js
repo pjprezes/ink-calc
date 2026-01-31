3de09e0a405ee7bc28683e95d6cab9afd08c3b08
pjprezes <pjprezes@gmail.com>
2026-01-31 09:39:40 +0100

    Update service worker to new cache version and assets

--- sw.js
+++ sw.js
@@ -1,16 +1,22 @@
-const CACHE_NAME = 'inkcalc-v1';
+const CACHE_NAME = 'inkcalc-v1.1';
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './app.js',
-  './manifest.json'
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
